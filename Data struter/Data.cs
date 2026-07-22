e3120a7b96d5a1ba275921abafacdc5846c22f86
Leen Zreaq <leenzreaq21@gmail.com>
2026-07-22 16:52:12 +0300

    add array list

--- Data struter/Data.cs
+++ Data struter/Data.cs
@@ -5,8 +5,25 @@ namespace Data
 {
     class Test
     {
+        // arraylist , linkedlist , hashing , set , hashmap , linked hash set , 
+        // tree map , linked hash map , 
         public static void Main(string[] args)
         {
+            List<int> arr = new List<int>();
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(x[i] + " ");
+            }
+
+            // to conver the list to array 
+            int[] x = arr.ToArray();
 
         }
     }
