1f4197bee1c483af319e4c3c25d9bba196cde4b4
Leen Zreaq <leenzreaq21@gmail.com>
2026-07-26 20:57:24 +0300

    eneric Constraints

--- Week_2/Day1.cs
+++ Week_2/Day1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 // Generics and advanced collections 
 namespace Week2
 {
+    // 1.1 , 1.2 => done 
     class Day1
     {
 
@@ -13,6 +14,16 @@ namespace Week2
             day.AddBook("arabic", "The Hobbit");
 
             Console.WriteLine(string.Join(", ", day.GetBookBy("arabic")));
+
+
+            GradeBook<int> grades = new GradeBook<int>();
+            grades.AddScore(85);
+            grades.AddScore(92);
+            grades.AddScore(70);
+            grades.AddScore(88);
+
+            Console.WriteLine("Highest: " + grades.GetHighestScore());
+            Console.WriteLine("Lowest: " + grades.GetLowestScore());
         }
 
         public void AddBook(string author, string title)
@@ -29,5 +40,44 @@ namespace Week2
             return bookByAuthor.TryGetValue(author, out var books) ? books : new List<string>();
         }
 
+    }
+
+    public class GradeBook<T> where T : IComparable<T>
+    {
+
+        private List<T> score = new List<T>();
+        public void AddScore(T scorest)
+        {
+            score.Add(scorest);
+        }
+
+        public T GetHighestScore()
+        {
+            T highest = score[0];
+
+            foreach (var scores in score)
+            {
+                if (scores.CompareTo(highest) > 0)
+                {
+                    highest = scores;
+                }
+            }
+            return highest;
+        }
+
+
+        public T GetLowestScore()
+        {
+            T lowest = score[0];
+
+            foreach (var scores in score)
+            {
+                if (scores.CompareTo(lowest) < 0)
+                {
+                    lowest = scores;
+                }
+            }
+            return lowest;
+        }
     }
 }
