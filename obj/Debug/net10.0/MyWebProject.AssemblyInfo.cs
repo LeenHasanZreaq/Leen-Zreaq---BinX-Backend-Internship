f5ac0437e2b4cd0ebb42878ca957e54d9d2b4dac
Leen Zreaq <leenzreaq21@gmail.com>
2026-08-07 23:02:27 +0300

    add the postman url /

--- obj/Debug/net10.0/MyWebProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyWebProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f76c921cf9b9555a589a9c28d7b51b9b4015e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a743e893808872915fc4e247ac434cea0608ac79")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
