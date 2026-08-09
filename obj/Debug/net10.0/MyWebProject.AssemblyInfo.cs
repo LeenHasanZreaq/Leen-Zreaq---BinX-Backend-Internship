23b3a73eeb3eca44e7c9f1513e062cb30d9956ca
Leen Zreaq <leenzreaq21@gmail.com>
2026-08-09 12:18:50 +0300

    some edits

--- obj/Debug/net10.0/MyWebProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyWebProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fb01435c452c45928370411ad56b069f5e52aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3a9e70a02cf230433c90582134e251f12caab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
